30cad76d14f7fd8b6ab12820973e9fefbb095b5a
AjtyCamp <50920184+AjtyCamp@users.noreply.github.com>
2019-06-28 14:55:17 +0200

    add styles and upgrade JS + clean JS

--- src/public/table.js
+++ src/public/table.js
@@ -66,6 +66,25 @@ var table = (function(){
             gui.hand.first.innerText = "";
             gui.hand.second.innerText = "";
             gui.percentage.innerText = "";
+
+            gui.table.tableCard1.classList.remove(self.game.table.begin[0].color);
+            gui.table.tableCard2.classList.remove(self.game.table.begin[1].color);
+            gui.table.tableCard3.classList.remove(self.game.table.begin[2].color);
+
+            gui.hand.first.classList.remove(self.game.hand[0].color);
+            gui.hand.second.classList.remove(self.game.hand[1].color);
+
+            gui.players.one.first.classList.remove(self.game.playerOne[0].color);
+            gui.players.one.second.classList.remove(self.game.playerOne[1].color);
+
+            gui.players.two.first.classList.remove(self.game.playerTwo[0].color);
+            gui.players.two.second.classList.remove(self.game.playerTwo[1].color);
+
+            gui.players.three.first.classList.remove(self.game.playerThree[0].color);
+            gui.players.three.second.classList.remove(self.game.playerThree[1].color);
+
+            gui.table.tableCard4.classList.remove(self.game.table.second.color);
+            gui.table.tableCard5.classList.add(self.game.table.third.color);
         }
 
         function startGame() {
@@ -74,23 +93,34 @@ var table = (function(){
             self.currentStep=1;
             console.log(self.game);
             gui.table.tableCard1.innerText = self.game.table.begin[0].card;
+            gui.table.tableCard1.classList.add(self.game.table.begin[0].color);
             gui.table.tableCard2.innerText = self.game.table.begin[1].card;
+            gui.table.tableCard2.classList.add(self.game.table.begin[1].color);
             gui.table.tableCard3.innerText = self.game.table.begin[2].card;
+            gui.table.tableCard3.classList.add(self.game.table.begin[2].color);
 
             gui.hand.first.innerText = self.game.hand[0].card;
+            gui.hand.first.classList.add(self.game.hand[0].color);
             gui.hand.second.innerText = self.game.hand[1].card;
+            gui.hand.second.classList.add(self.game.hand[1].color);
 
             if (gui.input.playersCount.value > 1) {
                 gui.players.one.first.innerText = self.game.playerOne[0].card;
                 gui.players.one.second.innerText = self.game.playerOne[1].card;
+                gui.players.one.first.classList.add(self.game.playerOne[0].color);
+                gui.players.one.second.classList.add(self.game.playerOne[1].color);
             }
             if (gui.input.playersCount.value > 2) {
                 gui.players.two.first.innerText = self.game.playerTwo[0].card;
                 gui.players.two.second.innerText = self.game.playerTwo[1].card;
+                gui.players.two.first.classList.add(self.game.playerTwo[0].color);
+                gui.players.two.second.classList.add(self.game.playerTwo[1].color);
             }
             if (gui.input.playersCount.value > 3) {
                 gui.players.three.first.innerText = self.game.playerThree[0].card;
                 gui.players.three.second.innerText = self.game.playerThree[1].card;
+                gui.players.three.first.classList.add(self.game.playerThree[0].color);
+                gui.players.three.second.classList.add(self.game.playerThree[1].color);
             }
 
             gui.percentage.innerText = game.calculateProbability(self.game, self.currentStep);
@@ -105,9 +135,11 @@ var table = (function(){
             if (self.currentStep > 2 || self.currentStep < 1) return;
             self.currentStep++;
             self.currentStep === 2 &&
-            (gui.table.tableCard4.innerText = self.game.table.second.card);
+            (gui.table.tableCard4.innerText = self.game.table.second.card,
+                gui.table.tableCard4.classList.add(self.game.table.second.color));
             self.currentStep === 3 &&
-            (gui.table.tableCard5.innerText = self.game.table.third.card);
+            (gui.table.tableCard5.innerText = self.game.table.third.card,
+                gui.table.tableCard5.classList.add(self.game.table.third.color));
 
             gui.percentage.innerText = game.calculateProbability(self.game, self.currentStep);
         }
